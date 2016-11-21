4042932eef1ccf3b0860702ce896f74aad40d88e
Nick Beukema <nick.beukema@gmail.com>
2016-11-20 20:00:54 -0500

    Build out data-graph component

--- frontend/app/components/data-graph.js
+++ frontend/app/components/data-graph.js
@@ -1,58 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  data: [],
 
-  timeSeriesLineContent: [
-    {
-      time: new Date(2013, 8, 16),
-      label: "Humidity",
-      value: 17326,
-      type: "percent"
-    }, {
-      time: new Date(2013, 8, 16),
-      label: "Temperature",
-      value: 4515,
-      type: "temperature"
-    }, {
-      time: new Date(2013, 8, 17),
-      label: "Humidity",
-      value: 15326,
-      type: "percent"
-    }, {
-      time: new Date(2013, 8, 17),
-      label: "Temperature",
-      value: 1515,
-      type: "temperature"
-    }, {
-      time: new Date(2013, 8, 18),
-      label: "Humidity",
-      value: 14326,
-      type: "percent"
-    }, {
-      time: new Date(2013, 8, 18),
-      label: "Temperature",
-      value: 8518,
-      type: "temperature"
-    }, {
-      time: new Date(2013, 8, 19),
-      label: "Humidity",
-      value: 42301,
-      type: "percent"
-    }, {
-      time: new Date(2013, 8, 19),
-      label: "Temperature",
-      value: 90191,
-      type: "temperature"
-    }, {
-      time: new Date(2013, 8, 20),
-      label: "Humidity",
-      value: 57326,
-      type: "percent"
-    }, {
-      time: new Date(2013, 8, 20),
-      label: "Temperature",
-      value: 39544,
-      type: "temperature"
-    }
-  ],
+  windowSize: 10,
+  dataWindow: Ember.computed('data.[]', 'windowSize', function(){
+    let data = this.get('data');
+    let windowSize = this.get('windowSize');
+    if(!windowSize) { windowSize = 1; }
+    return data.slice(Math.max(data.length - windowSize, 1));
+  }),
 });
