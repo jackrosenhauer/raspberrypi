2d97811d029ad254a7306adeee6ac2ccd7ebcdac
Nick Beukema <nick.beukema@gmail.com>
2016-11-28 19:57:32 -0500

    Cleanup data graph component

--- frontend/app/components/data-graph.js
+++ frontend/app/components/data-graph.js
@@ -34,26 +34,7 @@ export default Ember.Component.extend({
           text: this.get('yAxis')
         }
       }
-    }
+    };
   }),
 
-  chartData: [{
-    name: 'Jane',
-    data: [1, 0, 4]
-  }, {
-    name: 'John',
-    data: [5, 7, 3]
-  }],
-
-
-  windowSize: 10,
-  dataWindow: Ember.computed('data.[]', 'windowSize', function(){
-    let data = this.get('data');
-    let windowSize = this.get('windowSize');
-
-    if(!windowSize) { windowSize = 1; }
-    if(!data || data.length == 0) { return []; }
-
-    return data.slice(Math.max(data.length - windowSize, 1));
-  }),
 });
