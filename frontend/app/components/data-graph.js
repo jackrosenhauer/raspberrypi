a78fcd2bbdc550edf743afe53a827ff04c23aed4
Nick Beukema <nick.beukema@gmail.com>
2016-12-02 14:17:36 -0500

    add relay endpoint and add seed data

--- frontend/app/components/data-graph.js
+++ frontend/app/components/data-graph.js
@@ -10,6 +10,7 @@ export default Ember.Component.extend({
 
   theData: Ember.computed('highchart', function(){
     return [{
+      name: this.get('title'),
       data: this.get('highchart')
     }];
   }),
@@ -22,6 +23,9 @@ export default Ember.Component.extend({
       },
       title: {
         text: null
+      },
+      legend: {
+        enabled: false
       },
       xAxis: {
         type: 'datetime',
