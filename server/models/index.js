c10bc875df274f09ca9962fe914ace26276d1a03
Jack Rosenhauer II <jackrosenhauer@gmail.com>
2016-12-05 02:48:02 -0500

    added a flag to disable logging

--- server/models/index.js
+++ server/models/index.js
@@ -12,7 +12,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize('gardening_app', 'root', null, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
   });
 }
 
